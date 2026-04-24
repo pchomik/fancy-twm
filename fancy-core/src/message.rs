fb78e0fd628fad09d0289185131ecf852ef1d1fa
Pawel Chomicki <pawel.chomicki@gmail.com>
2026-04-24 22:28:22 +0200

    feat: move message types to a separate module for better organization

---
+++ fancy-core/src/message.rs
@@ -0,0 +1,17 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum Command {
+    MoveToNextVirtualDesktop,
+    MoveToPrevVirtualDesktop,
+    MoveToVirtualDesktop,
+    ChangeToNextVirtualDesktop,
+    ChangeToPrevVirtualDesktop,
+    ChangeToVirtualDesktop,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PipeMessage {
+    pub command: Command,
+    pub args: Option<Vec<String>>,
+}
